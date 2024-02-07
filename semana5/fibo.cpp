b999798e2e273938ac5f272c36eb055823eb593e
Edson Ticona Zegarra <eticonazegarra@gmail.com>
2024-02-07 20:10:10 +0100

    add fibo as memoization sample

---
+++ semana5/fibo.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int fibo(int n){
+  if ( n == 1 || n == 2 ) 
+    return 1;
+  return fibo(n-1)+fibo(n-2);
+}
+
+int main(){
+  int n;
+  cin >> n;
+  cout << fibo(n);
+}
